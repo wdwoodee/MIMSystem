a57408f586d9b56a509c4f63cb12d73adea5f57b
woodee <wdwoodee@163.com>
2018-06-30 21:28:30 +0800

    add new file utility

---
+++ MIMSystem.Utility/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIMSystem.Utility
+{
+    public class Utility
+    {
+        //public static string[] SplitString(string ss)
+        //{
+        //    string[] s = new string[]();
+
+        //    return s;
+        //}
+    }
+}
